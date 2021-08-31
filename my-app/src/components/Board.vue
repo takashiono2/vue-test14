dbdb4725f17f9fa43fcfc3157db92b44808e21b9
takashiono2 <takashiono710@gmail.com>
2021-09-01 01:22:31 +0900

    3章3-1

---
+++ my-app/src/components/Board.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+      my Trello
+    </header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+      <ListAdd/> 
+    </main>
+  </div>
+</template>
+
+<script>
+import ListAdd from './ListAdd.vue'
+export default{
+  components:{
+    ListAdd
+  }
+}
+</script>
---
+++ my-app/src/components/ListAdd.vue
@@ -0,0 +1,33 @@
+<template>
+  <form class="addlist" @submit.prevent="addList">
+    <input v-model="title" 
+      type="text" 
+      class="text-input"
+      placeholder="Add new list">
+    <button type="submit" class="add-button">
+      add
+    </button>
+  </form>
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      title:''
+    }
+  },
+  methods:{
+    addList(){
+      this.$store.dispatch('addlist',{
+        title: this.title
+      })
+      this.title=''
+    }
+  }
+}
+</script>
+
+ 
+ 
+ 
